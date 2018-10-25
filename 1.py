dec4fb5c55e446d334ec00782243167c7b0992ce
懿瑾先生 <752158139@qq.com>
2018-10-25 23:15:17 +0800

    wosad

--- 1.py
+++ 1.py
@@ -28,24 +28,24 @@ class MyRequestHandler(SocketServer.StreamRequestHandler):
             self.request.send(b"\n's'um or 't'imes or 'p'rint\n")
             c = self.request.recv(1024)[:-1]
             print c
-        if c == '1':
-            self.request.send(b'5 + 4 = ?\n')
-            c = self.request.recv(1024)[:-1]
-            if c == b'9':
-                self.request.send("ctf{sum}\n")
-            else:
-                self.request.send("wrong!\n")
+            if c == '1':
+                self.request.send(b'5 + 4 = ?\n')
+                c = self.request.recv(1024)[:-1]
+                if c == b'9':
+                    self.request.send("ctf{sum}\n")
+                else:
+                    self.request.send("wrong!\n")
                 
-        elif c == b'2':
-            self.request.send(b'3 * 2 = ?\n')
-            c = self.request.recv(1024)[:-1]  
-            if c == b'6':
-                self.request.send("ctf{times}\n")
-            else:
-                self.request.send("wrong!\n")            
-        elif c == b'3':
-            self.request.send("ctf{print}")
-        self.request.close()        
+            elif c == b'2':
+                self.request.send(b'3 * 2 = ?\n')
+                c = self.request.recv(1024)[:-1]  
+                if c == b'6':
+                    self.request.send("ctf{times}\n")
+                else:   
+                    self.request.send("wrong!\n")            
+            elif c == b'3':
+                self.request.send("ctf{print}")
+            self.request.close()        
         
 
 tcpServ = SocketServer.TCPServer(ADDR, MyRequestHandler)
